cbd9965d0cdcc05ca07010a2f279a95d1ae6e0b4
bakdaulet <bakosh21345@gmail.com>
2021-09-24 16:46:39 +0600

    a

--- izgilik/settings.py
+++ izgilik/settings.py
@@ -85,15 +85,15 @@ WSGI_APPLICATION = 'izgilik.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'dbiz',
