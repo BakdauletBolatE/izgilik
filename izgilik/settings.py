e99ba001bfdb4299337886bc7ee6eca92fe9f440
Diyar <djars500@gmail.com>
2021-09-24 16:37:07 +0600

    second commit

--- izgilik/settings.py
+++ izgilik/settings.py
@@ -16,7 +16,7 @@ try:
     from .local import *
     DEBUG = True
 except ImportError:
-    DEBUG = False
+    DEBUG = True
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
